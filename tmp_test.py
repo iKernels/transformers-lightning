94729ee34230663423e7db34523b5f249d9fccda
lucadiliello <luca.diliello@unitn.it>
2020-11-11 00:34:45 +0100

    debugging

--- tmp_test.py
+++ tmp_test.py
@@ -3,14 +3,15 @@ from argparse import Namespace
 
 import pytest
 import pytorch_lightning as pl
-from pytorch_lightning.core.datamodule import LightningDataModule
 import torch
-from torch.utils.data.dataset import Dataset, TensorDataset
-import transformers_lightning
+from pytorch_lightning.core.datamodule import LightningDataModule
 from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset, IterableDataset
 from transformers import AdamW, BertTokenizer
 from transformers.modeling_bert import (BertConfig,
                                         BertForSequenceClassification)
+
+import transformers_lightning
 
 n_cpus = multiprocessing.cpu_count()
 N = 20
@@ -29,7 +30,7 @@ class SimpleTransformerLikeModel(transformers_lightning.models.SuperModel):
 
     def training_epoch_end(self, outputs):
         ids = torch.cat([o['ids'] for o in outputs], dim=0)
-        print(ids); exit()
+
         try:
             received = torch.zeros((len(self.datamodule.train_dataset),))
         except TypeError:
@@ -46,6 +47,9 @@ class SimpleTransformerLikeModel(transformers_lightning.models.SuperModel):
         )
 
         print("ids: ", ids)
+
+    def training_end(self):
+        pass
 
     def validation_step(self, batch, batch_idx):
         kwargs = {k: batch[k] for k in ["input_ids", "attention_mask", "token_type_ids", "labels"]}
@@ -76,6 +80,88 @@ class ExampleDataset(Dataset):
         }
 
 
+
+
+
+
+class ExampleIterableDataset(IterableDataset):
+
+    @property
+    def length(self):
+        return N
+
+    def jump_forward(self, steps: int = 1):
+        """ Move reader forward and return last extracted element. """
+        row = None
+        for i in range(steps):
+            row = next(self.reader)
+        return row
+
+    """ Init is the same as super class """
+
+    def __iter__(self):
+        self.reader = range(100)
+        self.is_first = True
+        if hasattr(self, 'worker_info'):
+            delattr(self, 'worker_info') # it may be necessary to reload info after every epoch...
+
+        self.counter = 0
+
+        if self.is_distributed():
+            self.jump_forward(steps=self.get_distributed_id())
+
+        print(torch.utils.data.get_worker_info()); exit()
+
+        return self
+
+    # worker info
+    def get_worker_info(self):
+        if not hasattr(self, 'worker_info'):
+            self.worker_info = torch.utils.data.get_worker_info()
+        return self.worker_info
+
+    def is_distributed(self):
+        """ Return process id in [0, num_workers-1]! """
+        return self.get_worker_info() is not None
+
+    def get_distributed_id(self):
+        return self.get_worker_info().id
+    
+    def get_num_workers(self):
+        return self.get_worker_info().num_workers
+
+    def __next__(self):
+        """
+        Get next element.
+        Behaves differently based on whether distributed training is used.
+        """
+        if self.is_distributed():
+            # first step in distributed
+            if self.is_first:
+                self.is_first = False
+                row = self.jump_forward(steps=1)
+            # normal step in distributed
+            else:
+                row = self.jump_forward(steps=self.get_num_workers())
+
+        # normal step in single worker mode
+        else:
+            row = self.jump_forward(steps=1)
+
+        row_dict = self.get_data_as_dict(row)
+        row_dict = self.prepare(row_dict, idx=self.counter)
+
+        self.counter += 1
+
+        return row_dict
+
+
+
+
+
+
+
+
 class ExampleDataModule(LightningDataModule):
 
     def __init__(self, hparams):
@@ -83,7 +169,7 @@ class ExampleDataModule(LightningDataModule):
         self.hparams = hparams
 
     def setup(self, stage=None):
-        self.dataset = ExampleDataset(N)
+        self.dataset = ExampleIterableDataset(N)
 
     def train_dataloader(self):
         return DataLoader(self.dataset, batch_size=self.hparams.batch_size)
