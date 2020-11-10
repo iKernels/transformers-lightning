e57a9e3cbbcd8d267a08d8889d8da31071049e2c
lucadiliello <luca.diliello@unitn.it>
2020-11-10 19:38:49 +0100

    testing distributed datamodule

--- tests/test_datamodule.py
+++ tests/test_datamodule.py
@@ -248,7 +248,7 @@ def test_datamodule_gpu_dp(ds_type, num_workers, distributed_backend, gpus, epoc
 
 #    ['iter',     0,             'ddp',                  2,      1],
 #    ['iter',     2,             'ddp',                  2,      4],
-#    ['iter',     n_cpus,        'ddp',                  2,      10],
+    ['iter',     n_cpus,        'ddp',                  2,      10],
 
     # MAP dataset
     # num_workers with ddp
