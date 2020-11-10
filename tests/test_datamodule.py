ccb92b490e9065cf892092b3f935bca44dde5989
lucadiliello <luca.diliello@unitn.it>
2020-11-10 18:45:17 +0100

    testing distributed datamodule

--- tests/test_datamodule.py
+++ tests/test_datamodule.py
@@ -147,7 +147,7 @@ def test_datamodule_cpu(ds_type, num_workers, distributed_backend, gpus, epochs)
 
 
 
-# Test iter dataset work correctly
+# Test iter dataset work correctly with dp
 @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
 @pytest.mark.serial
 @pytest.mark.parametrize(
@@ -165,17 +165,6 @@ def test_datamodule_cpu(ds_type, num_workers, distributed_backend, gpus, epochs)
     ['iter',     2,             'dp',                   2,      4],
     ['iter',     n_cpus,        'dp',                   2,      10],
 
-    # num_workers with ddp
-    ['iter',     0,             'ddp',                  2,      2],
-    ['iter',     1,             'ddp',                  2,      2],
-    ['iter',     2,             'ddp',                  2,      2],
-    ['iter',     n_cpus,        'ddp',                  2,      2],
-
-    ['iter',     0,             'ddp',                  2,      1],
-    ['iter',     1,             'ddp',                  2,      2],
-    ['iter',     2,             'ddp',                  2,      4],
-    ['iter',     n_cpus,        'ddp',                  2,      10],
-
     # MAP dataset
     # num_workers with dp
     ['map',     0,             'dp',                   2,      2],
@@ -186,8 +175,75 @@ def test_datamodule_cpu(ds_type, num_workers, distributed_backend, gpus, epochs)
     ['map',     0,             'dp',                   2,      1],
     ['map',     1,             'dp',                   2,      2],
     ['map',     2,             'dp',                   2,      4],
-    ['map',     n_cpus,        'dp',                   2,      10],
+    ['map',     n_cpus,        'dp',                   2,      10]
+])
+def test_datamodule_gpu(ds_type, num_workers, distributed_backend, gpus, epochs):
+    
+    hparams = Namespace(
+        batch_size=4,
+        val_batch_size=4,
+        test_batch_size=4,
+        accumulate_grad_batches=3,
+        num_workers=num_workers,
+        dataset_dir='tests/test_data',
+        config_dir='tests/test_data',
+        cache_dir='cache',
+        output_dir='output',
+        max_epochs=epochs,
+        max_steps=None,
+        max_sequence_length=10,
+        gpus=gpus,
+        dataset_style=ds_type
+    )
 
+    if distributed_backend is not None:
+        hparams.distributed_backend = distributed_backend
+
+    # instantiate PL trainer
+    trainer = pl.Trainer.from_argparse_args(
+        hparams,
+        profiler=True,
+        logger=None,
+        callbacks=[],
+    )
+
+    # instantiate PL model
+    model = SimpleTransformerLikeModel(hparams)    
+
+    # Datasets
+    datamodule = ExampleDataModule(hparams, model, trainer)
+    
+    model.datamodule = datamodule
+    # Train!
+    if datamodule.do_train():
+        trainer.fit(model, datamodule=datamodule)
+
+    # Test!
+    if datamodule.do_test():
+        trainer.test(model, datamodule=datamodule)
+
+
+
+
+# Test iter dataset work correctly with dp
+@pytest.mark.skipif(torch.cuda.device_count() < 2, reason="test requires multi-GPU machine")
+@pytest.mark.serial
+@pytest.mark.parametrize(
+    ["ds_type", "num_workers", "distributed_backend", "gpus", "epochs"], [
+    
+    # ITER dataset
+    # num_workers with ddp
+    ['iter',     0,             'ddp',                  2,      2],
+#    ['iter',     1,             'ddp',                  2,      2],
+#    ['iter',     2,             'ddp',                  2,      2],
+#    ['iter',     n_cpus,        'ddp',                  2,      2],
+
+#    ['iter',     0,             'ddp',                  2,      1],
+#    ['iter',     1,             'ddp',                  2,      2],
+#    ['iter',     2,             'ddp',                  2,      4],
+#    ['iter',     n_cpus,        'ddp',                  2,      10],
+
+    # MAP dataset
     # num_workers with ddp
 #    ['map',     0,             'ddp',                  2,      2],
 #    ['map',     1,             'ddp',                  2,      2],
