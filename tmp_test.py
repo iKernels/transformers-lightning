d4da965b3800b6f7477e74c379a865d40a389703
lucadiliello <luca.diliello@unitn.it>
2020-11-12 01:40:04 +0100

    new simpler, faster and generator-based iterable dataset

--- tmp_test.py
+++ tmp_test.py
@@ -44,9 +44,10 @@ class SimpleTransformerLikeModel(transformers_lightning.models.SuperModel):
         try:
             received = torch.zeros((len(self.datamodule.train_dataset),)).to(dtype=bool)
         except TypeError:
-            received = torch.zeros(
-                (self.datamodule.train_dataset.limit if self.datamodule.train_dataset.limit is not None else self.datamodule.train_dataset.length,)
-            ).to(dtype=bool)
+            expected_len = (
+                self.datamodule.train_dataset.length // torch.distributed.get_world_size()
+            ) * torch.distributed.get_world_size()
+            received = torch.zeros((expected_len,)).to(dtype=bool)
         received[ids] = True
 
         # assert no duplicate element received
